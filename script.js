c1ee87b10d33802ff6428fb64ce8cbacddab095c
Edwin Salas <al24220158@utcj.edu.mx>
2025-02-25 21:11:44 -0700

    Agregando Proyecto Web EasyGym

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('btnPlan1').addEventListener('click', function() {
+    alert("¡Gracias por suscribirte al Plan 1! Aquí verás rutinas de entrenamiento.");
+});
+
+document.getElementById('btnPlan2').addEventListener('click', function() {
+    alert("¡Gracias por suscribirte al Plan 2! Aquí podrás ver rutinas y contactar a los entrenadores.");
+});
